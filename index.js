91c4765a77838282f76c4a9aaabe3be248369125
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-05 17:50:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function happyHolidays () {
   return('Happy holidays!')
 }
-function happyHolidaysTo('#name') {
-  return('Happy holidays, you!')
+function happyHolidaysTo($name) {
+  return('Happy holidays, `$name`!')
 }
 function happyHolidayTo('Independence Day', 'Isabel') {
   return('Happy holidays, you!')
